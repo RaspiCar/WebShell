9e3eddf4985cdd01d7f813629e0ff47d7a540074
fffonion <fffonion@gmail.com>
2014-11-10 19:30:12 +0800

    fix editor add CodeMirror config add License add document

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# CodeMirror
-[![Build Status](https://travis-ci.org/codemirror/CodeMirror.svg)](https://travis-ci.org/codemirror/CodeMirror)
-[![NPM version](https://img.shields.io/npm/v/codemirror.svg)](https://www.npmjs.org/package/codemirror)
+﻿#WebShell webui
 
-CodeMirror is a JavaScript component that provides a code editor in
-the browser. When a mode is available for the language you are coding
-in, it will color your code, and optionally help with indentation.
+A simple webshell for embedded systems 
 
-The project page is http://codemirror.net  
-The manual is at http://codemirror.net/doc/manual.html  
-The contributing guidelines are in [CONTRIBUTING.md](https://github.com/codemirror/CodeMirror/blob/master/CONTRIBUTING.md)
+Checkout `server` branch for server-end.
+
+###Run
+
+Use whatever CGI/1.1 compatible servers you like (eg. Boa, Apache).
+
+Build server binaries.
+
+Put webui files in DocumentRoot
+
+###Screenshot
+![preview](http://ww1.sinaimg.cn/large/005w9hKsjw1em64j77ep5j318g0p0tcw.jpg)
--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(function(){
     var dir='/';
 	var home;
 	var user;
+	var in_edit = false;
 	var PS1 = 'USER@webshell:DIR $ ';
     String.prototype.startsWith=function(str){
         var reg=new RegExp("^"+str);
@@ -18,17 +19,14 @@ $(function(){
         var reg=new RegExp(str+"$");
         return reg.test(this);
    	 }
-	$.post=function (e,r,i,s){return $.isFunction(r)&&(s=s||i,i=r,r=t),$.ajax({type:"POST",url:e,data:r,headers:{'X-PWD':dir},success:i,dataType:s})}
-	$.get=function (e,r,i,s){return v.isFunction(r)&&(s=s||i,i=r,r=t),$.ajax({type:"GET",url:e,headers:{'X-PWD':dir},data:r,success:i,dataType:s})}
+	$.post=function (e,r,i,err,s){return $.isFunction(r)&&(s=s||i,i=r,r=t),$.ajax({type:"POST",url:e,data:r,headers:{'X-PWD':dir},success:i,error:err,dataType:s})}
+	$.get=function (e,r,i,err,s){return $.isFunction(r)&&(s=s||i,i=r,r=t),$.ajax({type:"GET",url:e,headers:{'X-PWD':dir},data:r,success:i,error:err,dataType:s})}
 
 	function realBasename(p){
 		if(p==home) return '~';
-		else return p.split('/').pop();
+		else return p=='/'?'/':p.split('/').pop();
 	}
 
-	function buildPS1(){
-		return PS1.replace('USER', user).replace('DIR',realBasename(dir));
-	}
 
     //初始化
     $.post("/cmd/exec",{data:'echo ~'},function(response){
@@ -40,7 +38,7 @@ $(function(){
 			   {data:'whoami'},
 			   function(response){
 					user = response.replace('\n','');
-					$('#J_showBox').append(buildPS1());
+					appendToBox();
        		});
 		
 		/*$.post(
@@ -56,45 +54,70 @@ $(function(){
 
 	function editCode(filename){
         function sendFile(ctx,bol){
-            $.post(
-                '/cmd/savefile?',
-                {data:ctx},
-                function(res){
+        	console.log(filename);
+        	var bytesArray = new Uint8Array(ctx.length);
+			for (var i = 0, l = ctx.length; i < l; i++) {
+			  bytesArray[i] = ctx[i];
+			}
+            $.ajax({
+                url:'/cmd/savefile?'+filename,
+                type:'POST',
+                headers:{'X-PWD':dir},
+                contentType:'application/octet-stream',
+                data: ctx,
+   				processData: false,
+                success:function(res){
                     if(bol){
                         editor = null;
                         $('.CodeMirror').remove();
                         $('.button-group').hide();
                     }
+                    console.log('1232132131');
                     alert('保存成功！');
-            });
+            }});
         }
-    	$.get(
-       	 	'/cmd/readfile?',
-        	{data:ctx},
-        	function(res){
-                console.log(res);
-                $('#J_mirror').val(res);
-                editor = CodeMirror.fromTextArea(document.getElementById("J_mirror"), {
-                    mode: "python",
-                    lineNumbers: true
-                });
-                $('.button-group').show();
-                $('#J_save').on('click',function(){
-                    editor.save();
-                    sendFile($('#J_mirror').val(),false);
-                });
-                $('#J_saveAndBack').on('click',function(){
-                    editor.save();
-                    sendFile($('#J_mirror').val(),true);
+        if(!in_edit){
+			$.get(
+				'/cmd/readfile',
+				{data:filename},
+				function(res){
+					console.log(res);
+					//in_edit = true;
+					$('#J_mirror').val(res);
+					editor = CodeMirror.fromTextArea(document.getElementById("J_mirror"), {
+						mode: "python",
+						lineNumbers: true,
+						lineWrapping:true,
+						theme:"ambiance",
+						indentUnit:4,
+						autofocus:true
+					});
+					editor.setSize(800,400);
+					$('.button-group').show();
+					$('#J_save').on('click',function(){
+						editor.save();
+						sendFile($('#J_mirror').val(),false);
+					});
+					$('#J_saveAndBack').on('click',function(){
+						editor.save();
+						sendFile($('#J_mirror').val(),true);
 
-                });
-                $('#J_back').on('click',function(){
-                    hideOrShow(true);
-                    editor = null;
-                    $('.CodeMirror').remove();
-                    $('.button-group').hide();
-                })
-        });
+					});
+					$('#J_back').on('click',function(){
+						//hideOrShow(true);
+						editor = null;
+						$('.CodeMirror').remove();
+						$('.button-group').hide();
+					});
+					 appendToBox();
+					 $('html, body, .content').animate({scrollTop: $(document).height()}, 500);
+			},function(jqXHR, textStatus, errorThrown){
+					appendToBox('***An error occured.('+jqXHR.status+')***\n');
+			});
+        }else{
+        	alert("已经打开了文档");
+        }
+
 //		$.get('./editcode/'+filename,function(res){
 //            console.log(res);
 //            $('#J_mirror').val(res);
@@ -119,7 +142,34 @@ $(function(){
 //		});
 	}
 //    editCode('test.py');
+	function parsePath(p, newp){
+		if(newp.startsWith('~')) newp = home + newp.slice(1);//cd ~/xxx or cd ~
+		if(newp.startsWith('/')) return newp;//no else if becase newp may changed
+		//rstrip '/'
+		while(newp.endsWith('/')) newp = newp.slice(0, -1);
+		//else
+		console.log(newp+'---'+p);
+		var s = newp.split('/');
+		for(var t in s){
+			t = s[t];
+			if(t=='.' || t=='') continue;
+			else if(t=='..') p=p.split('/').slice(0, -1).join('/');
+			else p += ('/' + t);
+		}
+		if(p=='') p ='/';
+		return p;
+	}
 
+	function appendToBox(n){
+		var _ps1 = PS1.replace('USER', user).replace('DIR',realBasename(dir));
+		var _box = $('#J_showBox');
+		if(n){
+			if(!n.endsWith('\n')) n += '<span style="color:black;background:white">%</span><br/>';
+			_box.append(n.replace(/\n/g, "<br/>"));
+		}
+		_box.append('<b>'+_ps1+'</b>');
+		_box.scrollTop(_box.prop('scrollHeight'));
+	}
 	//监听键盘事件
 	$('#J_input').on('keydown',function(e){
 		switch(e.keyCode){
@@ -128,28 +178,44 @@ $(function(){
 				inputValue=$(this).val();
 				$(this).val('');
 				$('#J_showBox').append(path+inputValue+'<br>');
-				valueArr.push(inputValue);
+				if(valueArr.slice(-1)!=inputValue)   valueArr.push(inputValue);
 				pointer = valueArr.length;
-				console.log(inputValue.startsWith('cd'));
+				//console.log(inputValue.startsWith('cd'));
 				//将输入信息发送给服务器
                 if(inputValue.startsWith('edit')){
                     editCode(inputValue.split(' ').pop());
+                }else if (inputValue=='clear'){
+                	location.reload();
                 }else{
-                    $.post(
-                        "/cmd/exec",
-                        {data:inputValue},
-                        function(response){
-                            				//用户键入cd重新请求pwd
-                            if(inputValue.startsWith('cd')){
-								if(!response.replace('\n','')){//no error
-                                	var new_dir = response.split(' ').pop();
-									if(new_dir.startsWith('/')) dir = new_dir;
-									else dir += ('/' + new_dir);
+
+					if(inputValue.replace(' ', '') != ''){
+						$.post(
+							"/cmd/exec",
+							{data:inputValue},
+							function(response){
+								//用户键入cd重新请求pwd
+								if(inputValue.startsWith('cd')){
+									if(!response.replace('\n','')){//no error
+										//cd aa* is not supported
+										dir = parsePath(dir, inputValue.split(' ').pop());
+										//console.log(dir+','+response);
+										//$('#J_showBox').append('<b>'+buildPS1()+'</b>');
+										appendToBox();
+									}else{
+										appendToBox(response);
+									}
+								}else{
+									appendToBox(response);
 								}
-                            }else{
-                                $('#J_showBox').append(response.replace('\n','<br>')+'<br>'+buildPS1());
-                            }
-                    });
+							},
+							function(jqXHR, textStatus, errorThrown){
+								console.log('err');
+								appendToBox('***An error occured.('+jqXHR.status+')***\n');
+							});
+					}else{
+						appendToBox();
+					}
+					
                 }
 				break;
 
